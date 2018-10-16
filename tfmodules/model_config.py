84ec870c285adac1f9ff52a9c6d74950f32bbd46
jwkangmacpro2 <jwkang10@gmail.com>
2018-10-17 01:11:58 +0900

    num_stage =4

--- tfmodules/model_config.py
+++ tfmodules/model_config.py
@@ -105,7 +105,7 @@ class HourglassConfig(object):
     def __init__(self):
         self.updown_rate            = 2
         self.maxpool_kernel_size    =[3,3]
-        self.num_stage              = 5
+        self.num_stage              = 4
         self.center_conv_num        = 3
         self.skip_conv_num          = 4
         self.center_ch_num          = 32 #output channel num
